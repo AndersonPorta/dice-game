cd33c627c76769edb0f16d85445d55544cb20207
aportav <andi_idfer@hotmail.com>
2019-06-18 13:50:04 -0600

    Add functionality to the game

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-document.getElementById("title");
+// Assign random numbers
+var randomNumber1 = randomNum();
+var randomNumber2 = randomNum();
+
+// Select specific image using random number
+var randomImg1 = "images/dice" + randomNumber1 + ".png";
+var randomImg2 = "images/dice" + randomNumber2 + ".png";
+
+// Modify the src attribute of img
+document.querySelector(".img1").setAttribute("src", randomImg1);
+document.querySelector(".img2").setAttribute("src", randomImg2);
+
+// Show who won by modifying the heading
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🏆 Player 1 won!"
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 won! 🏆"
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
+
+// Generate random number between 1 and 6
+function randomNum() {
+    return Math.floor(Math.random() * 6) + 1;
+}
